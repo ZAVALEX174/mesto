955f302def07eac56cf3b1faea8fe18f74c5021c
Alexandr Zuev <zuevalex@yandex.ru>
2023-01-08 22:12:23 +0500

    revy2

--- script/script.js
+++ script/script.js
@@ -1,28 +1,31 @@
-"use stric";
-const openPopup = document.querySelector(".profile__popup-open");
+"use strict";
+const openPopup = document.querySelector(".profile__btn-editing");
 const closePopup = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
 
+let nameInput = document.querySelector(".popup__input_text_user");
+let jobInput = document.querySelector(".popup__input_text_job");
+let nameInputRec = document.querySelector(".profile__title");
+let jobInputRec = document.querySelector(".profile__subtitle");
+
 openPopup.addEventListener("click", function (e) {
-  e.preventDefault();
-  popup.classList.add("popup__active");
+  popup.classList.add("_active");
+
+  nameInput.value = nameInputRec.textContent;
+  jobInput.value = jobInputRec.textContent;
 });
 
 closePopup.addEventListener("click", function () {
-  popup.classList.remove("popup__active");
+  popup.classList.remove("_active");
 });
 
 const savePopup = document.querySelector(".popup__action");
 
 savePopup.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("popup__active");
-
-  let nameInput = document.querySelector(".popup__text-user");
-  let jobInput = document.querySelector(".popup__text-job");
-  let nameInputRec = document.querySelector(".profile__title");
-  let jobInputRec = document.querySelector(".profile__subtitle");
 
   nameInputRec.textContent = nameInput.value;
   jobInputRec.textContent = jobInput.value;
+
+  popup.classList.remove("_active");
 });
